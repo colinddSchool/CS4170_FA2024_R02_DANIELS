dffdf56487a90a205a0c2ed940ce4aaf5b4e5f33
Rob Green <greenr@bgsu.edu>
2018-10-09 09:52:59 -0400

    Updated template

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
-#include <omp.h>
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
 #include <iostream>
 
 int main(){
+
+    #ifdef _OPENMP
+        int myRank = omp_get_thread_num();
+        int numThreads = omp_get_num_threads();
+    #else
+        int myRank = 0;
+        int numThreads = 1;
+    #endif
 
 
     return 0;
