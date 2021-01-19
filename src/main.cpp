8b8c534c72935e267bc7402b12fa183a8aff6d87
Rob Green <greenr@bgsu.edu>
2021-01-19 07:22:12 -0500

    Updated main.cpp. Removed RNG

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
-#ifdef _OPENMP
-    #include <omp.h>
-#endif
-#include <iostream>
+#include <omp.h>
+#include <random>
+
+std::random_device rd;                           // only used once to initialise (seed) engine
+std::mt19937 rng(rd());                          // random-number engine used (Mersenne-Twister in this case)
+std::uniform_int_distribution<int> uni(0, 2000); // guaranteed unbiased
+
+std::vector<int> generateList(int n){
+
+    std::vector<int> retValue;
+    srandom(time(NULL));
+    for (int i = 0; i < n; i++){
+        retValue.push_back(uni(rng));
+    }
+
+    return retValue;
+}
 
 int main(){
 
-    #ifdef _OPENMP
-        int myRank = omp_get_thread_num();
-        int numThreads = omp_get_num_threads();
-    #else
-        int myRank = 0;
-        int numThreads = 1;
-    #endif
-
-
     return 0;
 }
+
+
