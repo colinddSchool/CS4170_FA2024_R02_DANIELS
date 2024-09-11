2b52b27f0a2470263ca5155f60cfd93425aadc53
Rob Green <robert.green@paireddonation.org>
2024-09-11 15:07:42 -0400

    Finished Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,72 @@
 #include <omp.h>
 #include <random>
+#include <iostream>
 
-std::random_device rd;                           // only used once to initialise (seed) engine
-std::mt19937 rng(rd());                          // random-number engine used (Mersenne-Twister in this case)
-std::uniform_int_distribution<int> uni(0, 2000); // guaranteed unbiased
+double f(double x) {
+    return x * x; // Example function: f(x) = x^2
+}
 
-std::vector<int> generateList(int n){
+double serialTrap(double a, double b, int n) {
 
-    std::vector<int> retValue;
-    srandom(time(NULL));
-    for (int i = 0; i < n; i++){
-        retValue.push_back(uni(rng));
+    double h, retValue, x;
+
+    h = (b - a) / n;
+    retValue = (f(a) + f(b)) / 2.0;
+
+    for (int i = 1; i < n; i++) {
+        x = a + i * h;
+        retValue += f(x);
     }
 
+    retValue *= h;
     return retValue;
 }
 
+void parallelTrap(double a, double b, int n, double &result){
+
+    double h, x, myRetValue;
+    int myId = omp_get_thread_num();
+    int numThreads = omp_get_num_threads();
+
+    int myN, myA, myB;
+
+    h = (b - a) / n;
+    
+    myN = n/numThreads;
+    myA = a + myId * myN * h;
+    myB = myA + myN * h;
+    myRetValue  = (f(myA) + f(myB)) / 2.0;
+
+    for (int i = 1; i < myN; i++) {
+        x = myA + i * h;
+        myRetValue += f(x);
+    }
+    myRetValue *= h;
+
+    #pragma omp critical
+    {
+        result += myRetValue;
+    }
+}
+
 int main(){
+
+    double result = 0;
+    double a, b;
+    int n;
+
+    a = 2;
+    b = 14;
+    n = 76;
+
+    result = serialTrap(a, b, n);
+    std::cout << "Serial Result: " << result << std::endl;
+
+    omp_set_num_threads(4);
+    result = 0.0;
+    #pragma omp parallel
+    parallelTrap(a, b, n, result);
+    std::cout << "Parallel Result: " << result << std::endl;
 
     return 0;
 }
