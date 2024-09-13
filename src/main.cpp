58862b72f9d55ab2c7fd3d8de3ab610aa01ce635
Rob Green <robert.green@paireddonation.org>
2024-09-13 18:55:40 +0000

    holy Moley it's Friday the 13th and it worked.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <omp.h>
 #include <random>
 #include <iostream>
+#include "CStopWatch.h"
 
 double f(double x) {
     return x * x; // Example function: f(x) = x^2
@@ -13,6 +14,7 @@ double serialTrap(double a, double b, int n) {
     h = (b - a) / n;
     retValue = (f(a) + f(b)) / 2.0;
 
+    #pragma omp parallel for reduction(+:retValue)
     for (int i = 1; i < n; i++) {
         x = a + i * h;
         retValue += f(x);
@@ -22,63 +24,34 @@ double serialTrap(double a, double b, int n) {
     return retValue;
 }
 
-double parallelTrap(double a, double b, int n){
-
-    double h, x, myRetValue;
-    int myId = omp_get_thread_num();
-    int numThreads = omp_get_num_threads();
-
-    int myN, myA, myB;
-
-    h = (b - a) / n;
-    
-    myN = n/numThreads;
-    myA = a + myId * myN * h;
-    myB = myA + myN * h;
-    myRetValue  = (f(myA) + f(myB)) / 2.0;
-
-    for (int i = 1; i < myN; i++) {
-        x = myA + i * h;
-        myRetValue += f(x);
-    }
-    myRetValue *= h;
-
-    return myRetValue;
-}
-
 int main(){
 
     double result = 0;
     double a, b;
     int n;
+    CStopWatch timer;
 
     a = 2;
     b = 14;
-    n = 76;
+    n = 40000;
 
+    #pragma omp parallel num_threads(1)
+
+    timer.startTimer();
     result = serialTrap(a, b, n);
+    timer.stopTimer();
     std::cout << "Serial Result: " << result << std::endl;
+    std::cout << "Serial Time:" << timer.getElapsedTime() << std::endl;
 
     omp_set_num_threads(4);
     result = 0.0;
-
-    // #pragma omp parallel
-    // parallelTrap(a, b, n, result);
-    // std::cout << "Parallel Result: " << result << std::endl;
-
     
     #pragma omp parallel num_threads(4)
-    {
-        // #pragma omp critical
-
-        double localResult = 0;
-        localResult = parallelTrap(a, b, n);
-
-        #pragma omp critical
-        result += localResult;
-    }
-    
+    timer.startTimer();
+    result = serialTrap(a, b, n);
+    timer.stopTimer();
     std::cout << "Parallel Result: " << result << std::endl;
+    std::cout << "Parallel Time:" << timer.getElapsedTime() << std::endl;
 
     return 0;
 }
